256fbedcd5eb7cdce084a0426ffef157a744a54b
victorbotsales <v.salesb@gmail.com>
2019-06-01 21:04:21 -0300

    boilerplate code

---
+++ backend/src/main/java/com/victor/arctouchmovies/ArctouchMoviesApplication.java
@@ -0,0 +1,33 @@
+package com.victor.arctouchmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ArctouchMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArctouchMoviesApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig implements WebMvcConfigurer {
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**");
+	    }
+	}
+}
---
+++ backend/src/test/java/com/victor/arctouchmovies/ArctouchMoviesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.victor.arctouchmovies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArctouchMoviesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
